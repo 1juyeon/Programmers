67d3f78eeabef63fb902ada86936493d5a4f6f6d
juyeon <110435623+1juyeon@users.noreply.github.com>
2025-06-10 23:53:16 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.09 ms, Memory: 4.19 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.c
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.c
@@ -6,6 +6,7 @@
 int** solution(int num_list[], size_t num_list_len, int n) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
     int** answer = (int**)malloc(150 * sizeof(int*));
+    
     for(int i = 0; i < num_list_len; i++)
         answer[i] = (int*)malloc(150 * sizeof(int));
     int m = 0;
